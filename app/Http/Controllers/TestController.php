16170c8fe583e9a793d364f8bd7c05d53c2cc0c4
sabu <sbruckpr@gmail.com>
2018-08-07 00:14:17 -0500

    Base project

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    //
+    public function welcome(){
+    	$a =5;
+    	$b =10;
+    	$c =$a+$b;
+    	return view('welcome');
+    	//return "El valor de la suma es $c";
+    }
+}
